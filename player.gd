171c081c7a696e0cfb3c17d1f4f35ec9dd38eac9
Lightnet <darkneter@gmail.com>
2017-09-03 21:07:49 -0700

    SImple chat server & client test.

--- player.gd
+++ player.gd
@@ -9,56 +9,31 @@ var jump = 10
 const SLOPE_SLIDE_STOP = 25.0
 const FLOOR_NORMAL = Vector3(0,1,0)
 
-var MOVE_LEFT = false
-var MOVE_RIGHT = false
-var MOVE_UP = false
-var MOVE_DOWN = false
+var dir = Vector3()
 
 func _ready():
 	set_process_input(true)
 	set_fixed_process(true)
+	dir.y = -1 * move_speed
 
 func _input(event):
-	if Input.is_action_just_pressed("move_up"):
-		MOVE_UP = true
-	if Input.is_action_just_released("move_up"):
-		MOVE_UP = false
-	if Input.is_action_just_pressed("move_down"):
-		MOVE_DOWN = true
-	if Input.is_action_just_released("move_down"):
-		MOVE_DOWN = false
 	pass
 	
 func _fixed_process(delta):
 	t = get_transform()
 	r = get_rotation()
-	#print(Input.is_action_just_pressed("move_up"))
 	
-	#var camera = get_node("Camera")
-	#camera.get
-	var dir = Vector3()
-	if MOVE_UP:
-		#move(Vector3(-1,0,0))
+	if Input.is_action_just_pressed("move_up"):
 		t.origin += t.basis[2] * -move_speed
 		dir = t.basis[2] * -move_speed
-		#print(dir)
-		#dir.y = 0.1
-		#move(dir)
-		#t.orthonormalized()
-		#move(t)
-	elif MOVE_DOWN:
-		#move(Vector3(1,0,0))
+	if Input.is_action_just_pressed("move_down"):
 		t.origin += t.basis[2] * move_speed
 		dir = t.basis[2] * move_speed
-		#dir.y = 0.1
-		#move(dir)
-		#t.orthonormalized()
-		#move(t)
-	else:
-		#dir.y = -0.5
+	if Input.is_action_just_released("move_up") or Input.is_action_just_released("move_down"):
+		dir.x = 0
 		dir.y = -1 * move_speed
-		#move(Vector3(0,-0.5,0))
-		pass
+		dir.z = 0
+	#print(dir)
 	#move(dir)
 	move_and_slide( dir, FLOOR_NORMAL, SLOPE_SLIDE_STOP )
 	
@@ -72,32 +47,4 @@ func _fixed_process(delta):
 		move(Vector3(0,1,0))
 	#set_transform(t)
 	set_rotation(r)
-		
-	#var motion = velocity * delta;
-	#motion = move(motion);
-	
-	if(self.is_on_floor()):
-		#print("Floor")
-		pass
-	else:
-		#print("AIR")
-		pass
-	#print(get_collision_count())
-	
-	
-	
-	#if (is_colliding()):
-		#var norm = get_collision_normal()
-		#var motion = motion.slide(norm)
-		#velocity = velocity.slide(norm)
-		#move(motion);
-		
-		
-		
-		
 	pass
-	
-	
-	
-	
-	
